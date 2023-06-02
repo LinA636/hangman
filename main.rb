7e6f1b03bda3f05e696301dcdadd3eab5e5c6abe
Lina <117147821+LinA636@users.noreply.github.com>
2023-06-02 12:11:05 +1200

    switch unless to until to make sure the wanted number is input

--- main.rb
+++ main.rb
@@ -3,14 +3,14 @@ require_relative 'hangman_game.rb'
 puts "start new game (1) or continue recent one (2)?:"
 new_or_old = gets.chomp
 
-unless [1,2].include?(new_or_old)
+until ["1","2"].include?(new_or_old)
   puts "start new game (1) or continue recent one (2)?:"
   new_or_old = gets.chomp
 end
 
-if new_or_old == 1
+if new_or_old == "1"
   HangmanGame.new().play
-elsif new_or_old == 2
+elsif new_or_old == "2"
   # load secret code and countdown 
   # set those
   # start_new_game
