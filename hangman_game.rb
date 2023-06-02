10de4536b168be66ff6e3deed9dbf9c2c4255d78
Lina <117147821+LinA636@users.noreply.github.com>
2023-06-02 14:43:49 +1200

    implement select_random_word-from_dict

--- hangman_game.rb
+++ hangman_game.rb
@@ -1,40 +1,49 @@
 class HangmanGame
   attr_accessor :secret_code, :countdown
 
-  def initialize(secret_code = select_word_from_dict(), countdown = 10)
+  def initialize(secret_code = select_random_word_from_dict(), countdown = 10)
     @secret_code = secret_code
     @countdown = countdown
   end
 
   def play()
-
-    # unless countdown == 0 do
-      # choice: pick letter(continue game), save game or quit game
-      unless countdown == 0
-        puts "Choose: quit game (q), save game (s) or pick a letter (p):"
+    until countdown == 0
+      puts "Choose: quit game (q), save game (s) or pick a letter (p):"
+      game_mode = gets.chomp
+      until ["q","s","p"].include?(game_mode)
+        puts "Choose: quit game (q), save game (s) or pick a new letter (l):"
         game_mode = gets.chomp
-        unless ["q","s","p"].include?(game_mode)
-          puts "Choose: quit game (q), save game (s) or pick a letter (p):"
-          game_mode = gets.chomp
-        end
-        if game_mode == "q"
-          quit_game()
-        elsif game_mode == "s"
-          save_game()
-        elsif game_mode == "p"
-          continue_game()
-        end
       end
+      if game_mode == "q"
+        quit_game()
+      elsif game_mode == "s"
+        save_game()
+      elsif game_mode == "l"
+        continue_game()
+      end
+    end
   end
 
   private
-  def select_word_from_dict()
-    # load directory: File.read(filename)
-    #
-    # select a word between 5 to 12 characters
-    # case-sensitive: all big characters
-    # safe each latter in hash: letter, and found (t/f)
-    # push each hash into an array
+  def select_random_word_from_dict()
+    possible_words = []
+    dict = File.open('google-10000-english-no-swears.txt')
+    dict.each_line do |line|
+      line.rstrip!.lstrip!
+      if 5 <= line.length && line.length <= 12
+        possible_words.push(line.upcase)
+      end
+    end
+
+    rand_word_letter_arr = possible_words[rand(possible_words.length)].split('')
+    help_hash_array = []
+    rand_word_letter_arr.each do |letter|
+      help_hash_array.push({
+        letter: letter,
+        found: false
+      })
+    end
+    return help_hash_array    
   end
 
   def print_display()
@@ -48,6 +57,7 @@ class HangmanGame
   end
 
   def continue_game()
+    puts "C"
     # self.secret_code= create_secret_code()
     # print_display()
     # pick_letter()
@@ -78,6 +88,7 @@ class HangmanGame
   end
 
   def save_game()
+    puts "S"
     # serialize game
     # safe: secret_code and countdown
     # save in file
@@ -85,8 +96,7 @@ class HangmanGame
   end
 
   def quit_game()
-    # delete progress
-    # exit
+    exit
   end
 
 end
